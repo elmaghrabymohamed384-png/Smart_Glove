cc00a47195264874459d67e5e5760160e2b331a0
elmaghrabymohamed384-png <elmaghrabymohamed384@gmail.com>
2026-09-18 16:17:44 +0300

    Delete Gloves_For_Deaf_people directory

--- Gloves_For_Deaf_people/codes_with_eclipse/HAL/VOICE/voice_interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef HAL_VOICE_VOICE_INTERFACE_H
-#define HAL_VOICE_VOICE_INTERFACE_H
-
-#include "../../lib/STD_Types.h"
-
-void VOICE_Init(void);
-void VOICE_Say(u8 Copy_u8PatternID);
-
-#endif
--- Gloves_For_Deaf_people/protues_simulation/readme.txt
+++
@@ -1,2 +0,0 @@
-first you need to add lib and model to hall and mpu6050 sensor 
-you wil find fils in lib_for_protues folder
